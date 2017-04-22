4d1e316d0c102aeb2a7db4a3c214c65f590e108a
nejos97 <jonathannenba@gmail.com>
2017-04-23 00:07:46 +0200

    last official update

---
+++ index.php
@@ -0,0 +1,308 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="assets/favicon/favicon.ico">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Pinohh Messenger Bot</title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/landing-page.css" rel="stylesheet"/>
+
+    </head>
+    <body class="landing-page landing-page1">
+        <nav class="navbar navbar-transparent navbar-top" role="navigation">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                    </button>
+                    <a href="https://pinohh.herokuapp.com">
+                        <div class="logo-container">
+                            <div class="logo">
+                                <img src="assets/img/logo_pinohh.png" alt="Pinohh Logo">
+                            </div>
+                            <div class="brand">
+                                Pinooh
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="example" >
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="https://facebook.com/pinohh">
+                            <i class="fa fa-facebook-square"></i>
+                            Like
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/nejos97/pinohh">
+                            <i class="fa fa-github"></i>
+                            Github
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:jonathannenba@gmail.com">
+                            <i class="fa fa-envelope"></i>
+                            Mail
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+        </nav>
+        <div class="wrapper">
+            <div class="parallax filter-gradient blue" data-color="blue">
+                <div class="parallax-background">
+                    <img class="parallax-background-image" src="assets/img/template/bg3.jpg">
+                </div>
+                <div class= "container">
+                    <div class="row">
+                        <div class="col-md-5 hidden-xs">
+                            <div class="parallax-image">
+                                <img class="phone" src="assets/img/template/iphone3.png" style="margin-top: 20px"/>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-md-offset-1">
+                            <div class="description">
+                                <h2><center>Pinohh Bot</center></h2>
+                                <br>
+                                <h5>Be amazed by the best looking bootstrap landing page on the web! Your new app deserves an amazing page to show all of its features. Clear visual, light colours and beautifully aligned elements - they all try to make the users aware of your great app features!</h5>
+                            </div>
+                            <div class="buttons">
+                                <center>
+                                  <a href="https://m.me/pinohh">
+                                    <img src="assets/img/contactus.png" style="width: 50%;border-radius:4px;" />
+                                  </a>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section section-gray section-clients">
+                <div class="container text-center">
+                    <h4 class="header-text">PINOHH STATISTICS</h4>
+                    <div class="row">
+                      <div class="col-lg-3">
+                        <h1>789</h1>
+                        <h5>Messages</h5>
+                      </div>
+                      <div class="col-lg-3">
+                        <h1>79</h1>
+                        <h5>Users</h5>
+                      </div>
+                      <div class="col-lg-3">
+                        <h1>39</h1>
+                        <h5>Birthday programming</h5>
+                      </div>
+                      <div class="col-lg-3">
+                        <h1>129</h1>
+                        <h5>Survey Vote</h5>
+                      </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section section-presentation">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="description">
+                                <h4 class="header-text">It's beautiful</h4>
+                                <p>And your app is also probably social, awesome, easy-to-use and vital to users. This is the place to enlist all the good things that your app has to share. Focus on the benefits that the uers will receive. Try to combine imaginery with text and show meaningful printscreens from your app, that will make it clear what exactly the basic functions are. </p>
+                                <p>Try to make it very clear for the people browsing the page that this product will enrich their life and will make a nice addition to the homescreen.
+                                <p>
+                            </div>
+                        </div>
+                        <div class="col-md-5 col-md-offset-1 hidden-xs">
+                            <img src="assets/img/template/mac.png"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="section section-demo">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div id="description-carousel" class="carousel fade" data-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="item">
+                                        <img src="assets/img/template/examples/home_33.jpg" alt="">
+                                    </div>
+                                    <div class="item active">
+                                        <img src="assets/img/template/examples/home_22.jpg" alt="">
+                                    </div>
+                                    <div class="item">
+                                        <img src="assets/img/template/examples/home_11.jpg" alt="">
+                                    </div>
+                                </div>
+                                <ol class="carousel-indicators carousel-indicators-blue">
+                                    <li data-target="#description-carousel" data-slide-to="0" class=""></li>
+                                    <li data-target="#description-carousel" data-slide-to="1" class="active"></li>
+                                    <li data-target="#description-carousel" data-slide-to="2" class=""></li>
+                                </ol>
+                            </div>
+                        </div>
+                        <div class="col-md-5 col-md-offset-1">
+                            <h4 class="header-text">Easy to integrate</h4>
+                            <p>
+                                With all the apps that users love! Make it easy for users to share, like, post and tweet their favourite things from the app. Be sure to let users know they continue to remain connected while using your app!
+                            </p>
+                            <a href="http://www.creative-tim.com/product/awesome-landing-page" id="Demo3" class="btn btn-fill btn-info" data-button="info">Get Free Demo</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section section-features">
+                <div class="container">
+                    <h4 class="header-text text-center">Features</h4>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="card card-blue">
+                                <div class="icon">
+                                    <i class="pe-7s-note2"></i>
+                                </div>
+                                <div class="text">
+                                    <h4>Online Customers Management</h4>
+                                    <p>All appointments sync with your Google calendar so your availability is always up to date. See your schedule at a glance from any device.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card card-blue">
+                                <div class="icon">
+                                    <i class="pe-7s-bell"></i>
+                                </div>
+                                <h4>Smart Notifications on hands</h4>
+                                <p>Automatic text and email reminders make sure customers always remember their upcoming appointments.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="card card-blue">
+                                <div class="icon">
+                                    <i class="pe-7s-graph1"></i>
+                                </div>
+                                <h4>Know your business better now</h4>
+                                <p>Take payments and run your business on the go, in your store and then see how it all adds up with analytics.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section section-testimonial">
+                <div class="container">
+                    <h4 class="header-text text-center">What people think</h4>
+                    <div id="carousel-example-generic" class="carousel fade" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                            <div class="item">
+                                <div class="mask">
+                                    <img src="assets/img/faces/face-4.jpg">
+                                </div>
+                                <div class="carousel-testimonial-caption">
+                                    <p>Jay Z, Producer</p>
+                                    <h3>"I absolutely love your app! It's truly amazing and looks awesome!"</h3>
+                                </div>
+                            </div>
+                            <div class="item active">
+                                <div class="mask">
+                                    <img src="assets/img/faces/face-3.jpg">
+                                </div>
+                                <div class="carousel-testimonial-caption">
+                                    <p>Drake, Artist</p>
+                                    <h3>"This is one of the most awesome apps I've ever seen! Wish you luck Creative Tim!"</h3>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <div class="mask">
+                                    <img src="assets/img/faces/face-2.jpg">
+                                </div>
+                                <div class="carousel-testimonial-caption">
+                                    <p>Rick Ross, Musician</p>
+                                    <h3>"Loving this! Just picked it up the other day. Thank you for the work you put into this."</h3>
+                                </div>
+                            </div>
+                        </div>
+                        <ol class="carousel-indicators carousel-indicators-blue">
+                            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+            <div class="section section-no-padding">
+                <div class="parallax filter-gradient blue" data-color="blue">
+                    <div class="parallax-background">
+                        <img class ="parallax-background-image" src="assets/img/template/bg3.jpg"/>
+                    </div>
+                    <div class="info">
+                        <h1>Go and Tchat with Pinohh </h1>
+                        <p>Powerful messenger Bot to help</p>
+                        <div class="buttons">
+                            <center>
+                              <a href="https://m.me/pinohh">
+                                <img src="assets/img/contactus.png" style="width: 50%;border-radius:4px;" />
+                              </a>
+                            </center>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer">
+                <div class="container">
+                    <nav class="pull-left">
+                        <ul>
+                            <li>
+                                <a href="https://pinohh.herokuapp.com">
+                                Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://pinohh.herokuapp.com/team">
+                                Team
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://pinohh.herokuapp.com/contact">
+                                Contact
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://pinohh.herokuapp.com/survey">
+                                Survey
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://pinohh.herokuapp.com/politics.php">
+                                politics & confidentiality
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div class="copyright">
+                        &copy; 2016 <a href="http://pinohh.herokuapp.com">Pinohh</a>, <i class="fa fa-code"></i> with lot of <i class="fa fa-coffee"></i> and <i class="fa fa-heart"></i> by <a href="https://github.com/nejos97">Nejos97</a></li>
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+    </body>
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/awesome-landing-page.js" type="text/javascript"></script>
+</html>
---
+++ politics.php
@@ -0,0 +1,169 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="assets/favicon/favicon.ico">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Welcome to Pinohh Privacy Policy </title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/landing-page.css" rel="stylesheet"/>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    </head>
+    <body class="landing-page landing-page1">
+        <nav class="navbar navbar-transparent navbar-top" role="navigation"  style="background:#4FC3F7">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                    </button>
+                    <a href="https://pinohh.herokuapp.com">
+                        <div class="logo-container">
+                            <div class="logo">
+                                <img src="assets/img/logo_pinohh.png" alt="Pinohh Logo">
+                            </div>
+                            <div class="brand">
+                                Pinooh
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="example" >
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="https://facebook.com/pinohh">
+                            <i class="fa fa-facebook-square"></i>
+                            Like
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/nejos97/pinohh">
+                            <i class="fa fa-github"></i>
+                            Github
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:jonathannenba@gmail.com">
+                            <i class="fa fa-envelope"></i>
+                            Mail
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+        </nav>
+        <br><br><br><br>
+        <div class="container">
+          <p>
+         	<h1>Welcome to Pinohh Privacy Policy </h1>
+         		<p>
+         			<h3> Pinohh Terms of Service </h3>
+         			<span><small> Last modified April 22, 2017</small></span> <br/>
+        		</p>
+         	 	<h3> What kind of information do we collect ? </h3>
+         	 	<p>
+         	 		Pinohh mainly needs to collect information about your current profile on Messenger, this include :
+         	 		<ul>
+         	 			<li>Your name: to interact with the bot, we need your name for best results.</li> <br/>
+         	 			<li>Your Messenger ID : When you're going to vote a survey we'll need this information to keep you vote into our database.</li> <br/>
+         	 			<li>Your friend list : we use this to schedule your friends anniversaries.</li> <br/>
+         	 			<li>Position : the bot will need your position in order to display the road traffic of your region, in particular for your City.</li>
+         	 		</ul>
+         	 	</p>
+
+         	 	<h3> How is this information shared ? </h3>
+         	 	 <p>We're not sharing your information with anyone.</p>
+
+         	 	<h3> How can I manage or delete information about 	me ?</h3>
+         	 		<p>
+         	 			Well, this is a very straightfoward operation. To do so, you can either contact admins by going through this <a href="http://pinohh.herokuapp.com/contact">link</a>  or by clicking on the persitent menu located at the bottom of text-area in messenger and click on "more".
+
+         	 		</p>
+
+         	 	<h3> How our global service operate </h3>
+         	 		<p>
+         	 			Pinohh admins may share information internally for statictis or to improve responses of the bot.
+         	 			We log interaction between you and the bot inside a file, and can delete it later or after your request.
+         	 		</p>
+
+         	 	<h3> How will we notify you of changes to this policy ? </h3>
+         	 		<p>
+         	 			We will notify you before we make changes to this policy and give you the opportunity to review and comment on the revised policy before continuing to use our Services.
+         	 		</p>
+
+         	 	<h3> How to conatact Pinohh with questions  </h3>
+         	 		<p>
+         	 			To learn more, you can contact Pinohh devs in this <a href="mailto:jonathanneba@gmail.com"> email</a> or this <a href="mailto: yagamidev@gmail.com"> one </a>, we'll be very proud to respond to your questions.
+         	 		</p>
+        </p>
+        </div>
+        <div class="section section-no-padding">
+            <div class="parallax filter-gradient blue" data-color="blue">
+                <div class="parallax-background">
+                    <img class ="parallax-background-image" src="assets/img/template/bg3.jpg"/>
+                </div>
+                <div class="info">
+                    <h1>Go and Tchat with Pinohh </h1>
+                    <p>Powerful messenger Bot to help</p>
+                    <div class="buttons">
+                        <center>
+                          <a href="https://m.me/pinohh">
+                            <img src="assets/img/contactus.png" style="width: 50%;border-radius:4px;" />
+                          </a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com">
+                            Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/team.php">
+                            Team
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/contact.php">
+                            Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/survey.php">
+                            Survey
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/politics.php">
+                            politics & confidentiality
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright">
+                    &copy; 2016 <a href="http://pinohh.herokuapp.com">Pinohh</a>, <i class="fa fa-code"></i> with lot of <i class="fa fa-coffee"></i> and <i class="fa fa-heart"></i> by <a href="https://github.com/nejos97">Nejos97</a></li>
+                </div>
+            </div>
+        </footer>
+      </div>
+    </body>
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/awesome-landing-page.js" type="text/javascript"></script>
+</html>
