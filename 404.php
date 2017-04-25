d2baf084be46962749ef50f546ce3b22cdf07800
nejos97 <jonathannenba@gmail.com>
2017-04-25 14:51:20 +0200

    add 404.php and .htaccess

---
+++ 404.php
@@ -0,0 +1,127 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <link rel="icon" type="image/png" href="assets/favicon/favicon.ico">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Welcome to Pinohh Privacy Policy </title>
+        <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+        <meta name="viewport" content="width=device-width" />
+
+        <link href="assets/css/bootstrap.css" rel="stylesheet" />
+        <link href="assets/css/landing-page.css" rel="stylesheet"/>
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+    </head>
+    <body class="landing-page landing-page1">
+        <nav class="navbar navbar-transparent navbar-top" role="navigation"  style="background:#4FC3F7">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button id="menu-toggle" type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar bar1"></span>
+                    <span class="icon-bar bar2"></span>
+                    <span class="icon-bar bar3"></span>
+                    </button>
+                    <a href="https://pinohh.herokuapp.com">
+                        <div class="logo-container">
+                            <div class="logo">
+                                <img src="assets/img/logo_pinohh.png" alt="Pinohh Logo">
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="example" >
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <a href="https://facebook.com/pinohh">
+                            <i class="fa fa-facebook-square"></i>
+                            Like
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://github.com/nejos97/pinohh">
+                            <i class="fa fa-github"></i>
+                            Github
+                            </a>
+                        </li>
+                        <li>
+                            <a href="mailto:jonathannenba@gmail.com">
+                            <i class="fa fa-envelope"></i>
+                            Mail
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+        </nav>
+        <br/><br/><br/><br/>
+        <div class="container">
+          <center>
+            <img src="assets/img/404.png" alt="4O4 Error"/>
+          </center>
+        </div>
+        <div class="section section-no-padding">
+            <div class="parallax filter-gradient blue" data-color="blue">
+                <div class="parallax-background">
+                    <img class ="parallax-background-image" src="assets/img/template/bg3.jpg"/>
+                </div>
+                <div class="info">
+                    <h1>Go and Tchat with Pinohh </h1>
+                    <p>Powerful messenger Bot to help</p>
+                    <div class="buttons">
+                        <center>
+                          <a href="https://m.me/pinohh">
+                            <img src="assets/img/contactus.png" style="width: 50%;border-radius:4px;" />
+                          </a>
+                        </center>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <nav class="pull-left">
+                    <ul>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com">
+                            Home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/team.php">
+                            Team
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/contact.php">
+                            Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/survey.php">
+                            Survey
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://pinohh.herokuapp.com/politics.php">
+                            policy and privacy
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div class="copyright">
+                    &copy; 2016 <a href="http://pinohh.herokuapp.com">Pinohh</a>, <i class="fa fa-code"></i> with lot of <i class="fa fa-coffee"></i> and <i class="fa fa-heart"></i> by <a href="https://github.com/nejos97">Nejos97</a></li>
+                </div>
+            </div>
+        </footer>
+      </div>
+    </body>
+    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+    <script src="assets/js/awesome-landing-page.js" type="text/javascript"></script>
+</html>
