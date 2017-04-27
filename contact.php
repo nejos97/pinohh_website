d76abd11bdbdda668b25c843878951b5081fd4ef
nejos97 <jonathannenba@gmail.com>
2017-04-27 19:02:51 +0200

    debug all

--- contact.php
+++ contact.php
@@ -124,10 +124,9 @@
             </div>
         </div>
         <?php include_once("footer.php"); ?>
-      </div>
+        <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+        <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
+        <script src="assets/js/bootstrap.js" type="text/javascript"></script>
+        <script src="assets/js/awesome-landing-page.js" type="text/javascript"></script>
     </body>
-    <script src="assets/js/jquery-1.10.2.js" type="text/javascript"></script>
-    <script src="assets/js/jquery-ui-1.10.4.custom.min.js" type="text/javascript"></script>
-    <script src="assets/js/bootstrap.js" type="text/javascript"></script>
-    <script src="assets/js/awesome-landing-page.js" type="text/javascript"></script>
 </html>
