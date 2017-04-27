30b0f189bd17d76a9cb8223a33f016569de2c0d2
nejos97 <jonathannenba@gmail.com>
2017-04-27 13:27:39 +0200

    update a footer

--- footer.php
+++ footer.php
@@ -24,13 +24,13 @@
                 </li>
                 <li>
                     <a href="https://pinohh.herokuapp.com/policy.php">
-                    politics & confidentiality
+                    Policy and Privacy
                     </a>
                 </li>
             </ul>
         </nav>
         <div class="copyright">
-            &copy; <?php echo date("Y") ?> <a href="http://pinohh.herokuapp.com">Pinohh</a>, <i class="fa fa-code"></i> with lot of <i class="fa fa-coffee"></i> and <i class="fa fa-heart"></i> by <a href="https://github.com/nejos97">Nejos97</a></li>
+            &copy; <?php echo date("Y") ?> <a href="http://pinohh.herokuapp.com">Pinohh</a>, <i class="fa fa-code"></i> with lot of <i class="fa fa-coffee"></i> and <i class="fa fa-heart"></i> by <a href="https://github.com/nejos97">Nejos97</a>
         </div>
     </div>
 </footer>
